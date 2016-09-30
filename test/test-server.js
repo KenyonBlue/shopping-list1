72e4c14e15ce3ed45a90d965abab1b90831e1da2
Kenyon <bluekenyfleo@gmail.com>
2016-09-30 01:26:10 +0000

    9292016926 update

--- test/test-server.js
+++ test/test-server.js
@@ -1,37 +1,30 @@
+global.DATABASE_URL = 'mongodb://localhost/shopping-list-test';
+
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+
 var server = require('../server.js');
+var Item = require('../models/item');
 
 var should = chai.should();
 var app = server.app;
-var storage = server.storage;
 
 chai.use(chaiHttp);
 
-
 describe('Shopping List', function() {
-    it('should add an item on POST', function(done) {
-        chai.request(app)
-            .post('/items')
-            .send({'name': 'Kale'})
-            .end(function(err, res) {
-                should.equal(err, null);
-                res.should.have.status(201);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                res.body.should.have.property('name');
-                res.body.should.have.property('id');
-                res.body.name.should.be.a('string');
-                res.body.id.should.be.a('number');
-                res.body.name.should.equal('Kale');
-                storage.items.should.be.a('array');
-                storage.items.should.have.length(4);
-                storage.items[3].should.be.a('object');
-                storage.items[3].should.have.property('id');
-                storage.items[3].should.have.property('name');
-                storage.items[3].id.should.be.a('number');
-                storage.items[3].name.should.be.a('string');
-                storage.items[3].name.should.equal('Kale');
+    before(function(done) {
+        server.runServer(function() {
+            Item.create({name: 'Broad beans'},
+                        {name: 'Tomatoes'},
+                        {name: 'Peppers'}, function() {
                 done();
             });
+        });
     });
+
+    after(function(done) {
+        Item.remove(function() {
+            done();
+        });
+    });
+});
